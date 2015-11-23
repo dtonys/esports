43c9cb4cdb59674c59fcbc3387f36c24c9fd9036
conix <tehmuffinman@gmail.com>
2015-11-22 20:25:47 -0800

    scraper takes args and can find scores/multiple pages; combined resolveMatch function; still working on trying to scrape finished results

--- backend/controllers/admintools.server.controller.js
+++ backend/controllers/admintools.server.controller.js
@@ -15,12 +15,18 @@ var PythonShell = require('python-shell'),
 exports.scrapeELS = function(req, res) {
   console.log('going');
 
+  //scrape for new matches.
   PythonShell.run('./scripts/scraper_esportlivescore.py',
-    {mode:'json'},
+    {
+      mode:'json',
+      args:['ty_finished.html', '1', '0']
+      //args:['ty_notstarted.html', '0', '7']
+    },
     function(err, results) {
       if (err) {
         return res.status(400).send({message: err, stacktrace:err.stack});
       } else {
+        /*
         var matchlist = [];
         var repeatcount = 0;
         for (var i = 0; i < results.length; i++)
@@ -29,18 +35,11 @@ exports.scrapeELS = function(req, res) {
 
             //if it's a non-empty object
             if (!_.isEmpty(cm_res))
-            {
               matchlist.push(cm_res);
-            }
             else
-            {
               repeatcount += 1;
-            }
 
-            if ((matchlist.length + repeatcount) >= results.length)
-            {
-              //console.log('results:' + results);
-
+            if ((matchlist.length + repeatcount) >= results.length) {
               console.log('Created ' + matchlist.length + " new matches.");
               console.log('Found ' + repeatcount + " repeated matches.");
 
@@ -49,12 +48,41 @@ exports.scrapeELS = function(req, res) {
 
           });
         }
+        */
+        for (var i = 0; i < results.length; i++)
+        {
+          var the_result = results[i].scraped_result;
+          console.log(JSON.stringify(results[i]));
+
+          var match_to_find = results[i];
+
+          delete results[i]['scraped_result'];
+          delete results[i]['matchStartTime'];
+          //match_to_find.matchStartTime = new Date(Date.parse(match_to_find.matchStartTime));
+          console.log(JSON.stringify(results[i]));
+
+          var processed = 0;
+          //need to copy everything except the thing
+          Match.find(results[i], function(found_matches) {
+            if (found_matches && found_matches.length > 0)
+            {
+              console.log('FOUND IT!!!!!!!!' + results[i]);
+              sbfuncs.resolveMatch(found_matches[0], the_result, req, res);
+            }
+            else
+            {
+              console.log("Match isn't in database.");
+            }
+
+            processed += 1;
+            if (processed >= results.length)
+            {
+              res.jsonp({});
+            }
+
+          });
+        }
+
     }
-
   });
-
-
-  //var path = 'ty_notstarted.html';
-  //
-  //scrapeELSPage(path);
 };
